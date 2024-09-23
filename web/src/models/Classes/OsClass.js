074f327ec47d91605270fdaee5527e88563b183d
HenryFSilva <henryfernando2612@gmail.com>
2024-09-23 19:48:34 -0300

    Models - Classes

---
+++ web/src/models/Classes/OsClass.js
@@ -0,0 +1,26 @@
+class os{
+    constructor(pOs){
+        this.id = (pOs.id !== null || pOs.id > 0) ? pOs.id : null;
+        this.data = pOs.data;
+        this.status = (pOs.status !== null || pOs.status != '') ? pOs.status : null;
+        this.orçamento = pOs.orçamento;
+        this.tbl_vaiculo_id = (pOs.tbl_vaiculo_id !== null || pOs.tbl_vaiculo_id > 0) ? pOs.tbl_vaiculo_id : null;
+        this.tbl_vaiculo_tbl_pessoa_id = (pOs.tbl_vaiculo_tbl_pessoa_id !== null || pOs.tbl_vaiculo_tbl_pessoa_id > 0) ? pOs.tbl_vaiculo_tbl_pessoa_id : null;
+    }
+    get Id(){return this.id;}
+    set Id(value){this.id = value;}
+
+    get Data(){return this.data;}
+    set Data(value){this.data = value;}
+
+    get Status(){return this.status;}
+    set Status(value){this.status = value;}
+
+    get Tbl_vaiculo_id(){return this.tbl_vaiculo_id;}
+    set Tbl_vaiculo_id(value){this.tbl_vaiculo_id = value;}
+
+    get Tbl_vaiculo_tbl_pessoa_id(){return this.tbl_vaiculo_tbl_pessoa_id;}
+    set Tbl_vaiculo_tbl_pessoa_id(value){this.tbl_vaiculo_tbl_pessoa_id = value;}
+}
+
+module.exports = os;
--- web/src/models/Classes/PessoaClass.js
+++ web/src/models/Classes/PessoaClass.js
@@ -1,1 +1,26 @@
-console.log("inicio")
+class Pessoa {
+    constructor(pPes) {
+        this.id = (pPes.id !== null || pPes.id > 0) ? pPes.id : null;
+        this.nome = pPes.nome;
+        this.cpf = pPes.cpf;
+        this.email = pPes.email;
+        this.tipo = pPes.tipo;
+    }
+    get Id(){return this.id;}
+    set Id(value){this.id = value;}
+
+    get Nome() { return this.nome; }
+    set Nome(value) { this.nome = value; }
+
+    get Cpf() { return this.cpf; }
+    set Cpf(value) { this.cpf = value; }
+
+    get Email() { return this.email; }
+    set Email(value) { this.email = value; }
+
+    get Tipo() { return this.tipo; }
+    set Tipo(value) { this.tipo = value; }
+
+}
+
+module.exports = Pessoa;
