1f5c4cc01101077d638845ae0bca49ed2726246f
Luiz Eduardo <le7431474@gmail.com>
2024-10-04 20:42:33 -0300

    Finalização estilização geral Historico

--- mobile/mecanica/src/pages/historico/index.js
+++ mobile/mecanica/src/pages/historico/index.js
@@ -1,13 +1,40 @@
 import { LinearGradient } from "expo-linear-gradient"
-import { StyleSheet, Platform, StatusBar, View, Text, TouchableOpacity } from "react-native"
+import { StyleSheet, Platform, StatusBar, View, Text, TouchableOpacity, TextInput } from "react-native"
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons"
 export default function Historico() {
     return (
         <LinearGradient colors={
             ['#000000', 'rgba(0, 0, 0, 0.5)']}
             style={styles.androidSafeArea}>
-
+            <View style={styles.inputBusca}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Buscar por placa"
+                >
+                </TextInput>
+            </View>
             <View style={styles.container}>
+
+                <View style={styles.historicoItem}>
+                    <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
+                    <View style={styles.alinha}>
+                        <Text style={styles.textDados}>20/11/2024</Text>
+                        <Text style={styles.textDados}>R$343,50</Text>
+                    </View>
+                    <TouchableOpacity style={styles.icon}>
+                        <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.historicoItem}>
+                    <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
+                    <View style={styles.alinha}>
+                        <Text style={styles.textDados}>20/11/2024</Text>
+                        <Text style={styles.textDados}>R$343,50</Text>
+                    </View>
+                    <TouchableOpacity style={styles.icon}>
+                        <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
+                    </TouchableOpacity>
+                </View>
                 <View style={styles.historicoItem}>
                     <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
                     <View style={styles.alinha}>
@@ -43,8 +70,7 @@ const styles = StyleSheet.create({
         width: '100%',
         marginTop: 10,
         position: 'relative',
-        // Sombras para Android
-        elevation: 5, // Controla a intensidade da sombra
+        elevation: 5,
     },
     textVeiculo: {
         fontWeight: "bold",
@@ -70,5 +96,22 @@ const styles = StyleSheet.create({
         backgroundColor: "red",
         borderRadius: 5,
         paddingHorizontal: 10,
-    }
+    },
+    inputBusca: {
+        backgroundColor: "#fff",
+        width: "90%",
+        height: 40,
+        borderRadius: 25,
+        flexDirection: "row",
+        alignItems: "center",
+        paddingHorizontal: 20,
+        alignSelf: "center",
+        shadowColor: "#000",
+    },
+    input: {
+        flex: 1,
+        fontSize: 16,
+        paddingVertical: 10,
+    },
 });
+
