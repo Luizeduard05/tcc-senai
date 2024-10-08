65a2f3b7350a946da47577a2b55efa6fd09882ed
Luiz Eduardo <le7431474@gmail.com>
2024-10-08 19:05:05 -0300

    estrutura inicial navegaçao mecanico

---
+++ mobile/mecanica/src/pages/Mecanico/novoOrcamento/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function NovoOrcamentoMecanico() {
+    return (
+        <View>
+            <Text>Novo Orçamento Mecânico</Text>
+        </View>    
+    )
+}
---
+++ mobile/mecanica/src/pages/Mecanico/visualizaOrcamento/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function VisualizaOrcamentoMecanico() {
+    return (
+        <View>
+            <Text>Visualiza Orçamento Mecânico</Text>
+        </View>    
+    )
+}
