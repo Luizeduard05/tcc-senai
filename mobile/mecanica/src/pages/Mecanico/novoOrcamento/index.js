f3a675cabeb83118c85f41cc82763e7653eb0cfc
Luiz Eduardo <le7431474@gmail.com>
2024-10-08 21:47:38 -0300

    atualiza

--- mobile/mecanica/src/pages/Mecanico/novoOrcamento/index.js
+++ mobile/mecanica/src/pages/Mecanico/novoOrcamento/index.js
@@ -1,9 +1,86 @@
-import { View, Text } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, Platform, StatusBar, View, Text, TextInput, TouchableOpacity } from "react-native";
+
 
 export default function NovoOrcamentoMecanico() {
     return (
-        <View>
-            <Text>Novo Orçamento Mecânico</Text>
-        </View>    
+        <LinearGradient
+            colors={['#000000', 'rgba(0, 0, 0, 0.5)']}
+            style={styles.androidSafeArea}
+        >
+            <View style={styles.container}>
+
+                <Text>Data</Text>
+                <TextInput>
+                </TextInput>
+
+                <Text>Placa</Text>
+                <TextInput>
+                </TextInput>
+
+                <Text>Observação</Text>
+                <TextInput>
+                </TextInput>
+
+                <Text>Orçamento</Text>
+                <TextInput>
+                </TextInput>
+
+                <TouchableOpacity style={styles.btnConfirmar} >
+                    <Text style={styles.textBtn}>Confirmar</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnCancelar} >
+                    <Text style={styles.textBtnCancelar}>Cancelar</Text>
+                </TouchableOpacity>
+            </View>
+        </LinearGradient>
     )
 }
+
+const styles = StyleSheet.create({
+    androidSafeArea: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    },
+    container: {
+        flex: 0,
+        margin: 20,
+        height: '80%',
+        backgroundColor: "#383838",
+        borderRadius: 10,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    btnConfirmar: {
+        width: "70%",
+        height: 50,
+        backgroundColor: "#FFF",
+        justifyContent: "center",
+        // marginBottom: 30,
+        alignItems: "center",
+        borderRadius: 10,
+        marginTop: 20
+    },
+    btnCancelar: {
+        width: "70%",
+        height: 50,
+        backgroundColor: "#FF0000",
+        justifyContent: "center",
+
+        alignItems: "center",
+        borderRadius: 10,
+        marginTop: 20
+    },
+    textBtnCancelar: {
+        fontSize: 20,
+        color: "#fff",
+        fontWeight: "bold",
+    },
+    textBtn: {
+        fontSize: 20,
+        color: "#000000",
+        fontWeight: "bold",
+    }
+})
--- mobile/mecanica/src/pages/Mecanico/visualizaOrcamento/index.js
+++ mobile/mecanica/src/pages/Mecanico/visualizaOrcamento/index.js
@@ -1,9 +1,94 @@
-import { View, Text } from "react-native";
-
+import { LinearGradient } from "expo-linear-gradient"
+import { StyleSheet, Platform, StatusBar, View, Text, TouchableOpacity, TextInput } from "react-native"
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons"
 export default function VisualizaOrcamentoMecanico() {
     return (
-        <View>
-            <Text>Visualiza Orçamento Mecânico</Text>
-        </View>    
+        <LinearGradient colors={
+            ['#000000', 'rgba(0, 0, 0, 0.5)']}
+            style={styles.androidSafeArea}>
+            <View style={styles.container}>
+
+                <View style={styles.historicoItem}>
+                    <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
+                    <View style={styles.alinha}>
+                        <Text style={styles.textDados}>20/11/2024</Text>
+                        <Text style={styles.textDados}>R$343,50</Text>
+                    </View>
+                    <TouchableOpacity style={styles.icon}>
+                        <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.historicoItem}>
+                    <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
+                    <View style={styles.alinha}>
+                        <Text style={styles.textDados}>20/11/2024</Text>
+                        <Text style={styles.textDados}>R$343,50</Text>
+                    </View>
+                    <TouchableOpacity style={styles.icon}>
+                        <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.historicoItem}>
+                    <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
+                    <View style={styles.alinha}>
+                        <Text style={styles.textDados}>20/11/2024</Text>
+                        <Text style={styles.textDados}>R$343,50</Text>
+                    </View>
+                    <TouchableOpacity style={styles.icon}>
+                        <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
+                    </TouchableOpacity>
+                </View>
+            </View>
+
+        </LinearGradient>
     )
 }
+const styles = StyleSheet.create({
+    androidSafeArea: {
+        flex: 1,
+        justifyContent: "center",
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    },
+    container: {
+        flex: 1,
+        justifyContent: "flex-start",
+        padding: 15,
+    },
+    historicoItem: {
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        padding: 25,
+        paddingTop: 10,
+        paddingBottom: 30,
+        width: '100%',
+        marginTop: 10,
+        position: 'relative',
+        elevation: 5,
+    },
+    textVeiculo: {
+        fontWeight: "bold",
+        fontSize: 20,
+        paddingBottom: 10
+    },
+    textDados: {
+        color: "#5B5B5B",
+        fontSize: 18
+    },
+    alinha: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginRight: 80
+    },
+    icon: {
+        position: 'absolute',
+        right: 0,
+        top: 0,
+        bottom: 0,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "red",
+        borderRadius: 5,
+        paddingHorizontal: 10,
+    },
+});
+
