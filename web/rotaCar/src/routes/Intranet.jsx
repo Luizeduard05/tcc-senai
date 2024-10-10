9825f02863d83f560950f32f3498560e274aae85
Vinicius Fidelis <vinicius.curso.tecnico.senai@gmail.com>
2024-10-10 20:37:44 -0300

    Atualizando Front Intranet e historico

--- web/rotaCar/src/routes/Intranet.jsx
+++ web/rotaCar/src/routes/Intranet.jsx
@@ -1,5 +1,5 @@
 
-import "./Intranet.css"
+import styles from "./Intranet.module.css"
 
 import logoCarro from "../assets/logoCarro.png"
 import CarrosImg from "../assets/carros-imagem.png"
@@ -8,21 +8,21 @@ import CarroCivic from "../assets/honda-civic.png"
 
 const Intranet = () => {
     return (
-        <div className="container">
-        <div className="header">
-          <div className="logo">
+        <div className={styles.container}>
+        <div className={styles.header}>
+          <div className={styles.logo}>
           <img src={logoCarro} alt="" />
           </div>
         </div>
-        <div className="contentVn">
-          <div className="card">
-            <div className="text">
+        <div className={styles.contentVn}>
+          <div className={styles.card}>
+            <div className={styles.text}>
               <h1>Olá Vinícius!</h1>
               <h2>Como podemos ajudar hoje?</h2>
             </div>
-            <div className="buttons">
-              <button className="button">Orçamentos</button>
-              <button className="button">Agendamentos</button>
+            <div className={styles.buttons}>
+              <button className={styles.button}>Orçamentos</button>
+              <button className={styles.button}>Agendamentos</button>
             </div>
            
             <img className="carros" src={CarroCivic} alt="" />
