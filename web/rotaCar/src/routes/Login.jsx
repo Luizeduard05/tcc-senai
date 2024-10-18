34e901e3ea91f0f71b6817603b3fb52f11a36a09
Joao1721 <joaopaulolimadesouza180@gmail.com>
2024-10-18 20:06:18 -0300

    consumindo api

--- web/rotaCar/src/routes/Login.jsx
+++ web/rotaCar/src/routes/Login.jsx
@@ -1,12 +1,51 @@
 import { Link } from 'react-router-dom'
 import styleLogin from './Login.module.css'
+import api from '../../service/api'
+import { useState } from 'react'
 
 const Login = () => {
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [token, setToken] = useState('')
+    const [perfil, setPerfil] = useState('')
+
+    const handleChangeEmail = (e) => {
+        setEmail(e.target.value);
+        console.log(email)
+
+        
+    }
+
+    const handleChangeSenha = (e) => {
+        setSenha(e.target.value);
+        console.log(email)
+
+        
+    }
+
+    const hendleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await api.post('/login', { email, senha });
+            setToken(response.data.token);
+            setPerfil(response.data.perfil);
+            console.log(response.data)
+            console.log(setToken);
+        } catch (error) {
+            console.log(error);
+
+        }
+
+    }
+
+
+
+
     return (
         <>
             <section className={styleLogin.PaiLogin}>
                 <section className={styleLogin.conteinerLoginn}>
-                <span></span>
+                    <span></span>
                     <span></span>
                     <span></span>
                     <span></span>
@@ -272,25 +311,25 @@ const Login = () => {
                             <h2>Bem vindo de volta</h2>
                             <div className={styleLogin.loginForm}>
                                 <div className={styleLogin.inputWrapper}>
-                                    <input type="email" required />
+                                    <input type="email" name='email' value={email} onChange={handleChangeEmail} required />
                                     <i>email</i>
                                 </div>
                                 <div className={styleLogin.inputWrapper}>
-                                    <input type="password" required />
+                                    <input type="password" name='senha' value={senha} onChange={handleChangeSenha} required />
                                     <i>senha</i>
                                 </div>
                                 <div className={styleLogin.loginLinks}>
-                                  
+
                                     <Link to={"/cadastro"}>
                                         cadastre-se
                                     </Link>
-                                    
+
                                 </div>
                                 <div className={styleLogin.inputWrapper}>
-                                    <Link to={"/intranet"}>
-                                    <input type="submit" value="Entrar" />
-                                    </Link>
-                                    
+                                    {/* <Link to={"/intranet"}> */}
+                                        <input type="submit"  value='Entrar' onSubmit={hendleSubmit} />
+                                    {/* </Link> */}
+
                                 </div>
                             </div>
                         </div>
@@ -302,7 +341,9 @@ const Login = () => {
 
 
         </>
-    )
+    );
 }
 
 export default Login
+
+ 
