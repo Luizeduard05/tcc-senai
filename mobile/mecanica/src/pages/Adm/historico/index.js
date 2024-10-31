6765fc92fab8bb6319ffa90b666e443e1249d6c7
Luiz Eduardo <le7431474@gmail.com>
2024-10-30 21:41:03 -0300

    Requisição de chamada de orçamentos

--- mobile/mecanica/src/pages/Adm/historico/index.js
+++ mobile/mecanica/src/pages/Adm/historico/index.js
@@ -1,56 +1,60 @@
-import { LinearGradient } from "expo-linear-gradient"
-import { StyleSheet, Platform, StatusBar, View, Text, TouchableOpacity, TextInput } from "react-native"
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons"
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, Platform, StatusBar, View, Text, TouchableOpacity, TextInput } from "react-native";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import { useAuth } from "../../../context/AuthContext";
+import { useEffect, useState } from "react";
+import api from "../../../services/api/api";
 
 export default function HistoricoADM() {
+    const { token } = useAuth();
+    const [historico, setHistorico] = useState([]);
+
+    const getOrcamentos = async () => {
+        try {
+            const response = await api.get("/orcamentos", {
+                headers: {
+                    Authorization: `Token ${token}`
+                }
+            });
+            console.log(response.data);
+            setHistorico(response.data.ordensServico);  // Armazena apenas ordensServico
+        } catch (error) {
+            console.log("Erro ao buscar histórico:", error);
+        }
+    };
+
+    useEffect(() => {
+        getOrcamentos();
+    }, []);
+
     return (
-        <LinearGradient colors={
-            ['#000000', 'rgba(0, 0, 0, 0.5)']}
-            style={styles.androidSafeArea}>
+        <LinearGradient colors={['#000000', 'rgba(0, 0, 0, 0.5)']} style={styles.androidSafeArea}>
             <View style={styles.inputBusca}>
                 <TextInput
                     style={styles.input}
                     placeholder="Buscar por placa"
-                >
-                </TextInput>
+                />
             </View>
             <View style={styles.container}>
-
-                <View style={styles.historicoItem}>
-                    <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
-                    <View style={styles.alinha}>
-                        <Text style={styles.textDados}>20/11/2024</Text>
-                        <Text style={styles.textDados}>R$343,50</Text>
+                {historico.map((item) => (
+                    <View key={item.id_os} style={styles.historicoItem}>
+                        <Text style={styles.textVeiculo}>Veículo: {item.placa}</Text>
+                        <Text style={styles.textVeiculo}>Modelo: {item.modelo}</Text>
+                        <Text style={styles.textVeiculo}>Email Cliente: {item.email}</Text>
+                        <View style={styles.alinha}>
+                            <Text style={styles.textDados}>Data: {item.data}</Text>
+                            <Text style={styles.textDados}>Total: R${item.total}</Text>
+                        </View>
+                        <TouchableOpacity style={styles.icon}>
+                            <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
+                        </TouchableOpacity>
                     </View>
-                    <TouchableOpacity style={styles.icon}>
-                        <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.historicoItem}>
-                    <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
-                    <View style={styles.alinha}>
-                        <Text style={styles.textDados}>20/11/2024</Text>
-                        <Text style={styles.textDados}>R$343,50</Text>
-                    </View>
-                    <TouchableOpacity style={styles.icon}>
-                        <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.historicoItem}>
-                    <Text style={styles.textVeiculo}>Veiculo: UTS-0000</Text>
-                    <View style={styles.alinha}>
-                        <Text style={styles.textDados}>20/11/2024</Text>
-                        <Text style={styles.textDados}>R$343,50</Text>
-                    </View>
-                    <TouchableOpacity style={styles.icon}>
-                        <MaterialCommunityIcons name="clipboard-text-multiple-outline" size={32} color="white" />
-                    </TouchableOpacity>
-                </View>
+                ))}
             </View>
-
         </LinearGradient>
-    )
+    );
 }
+
 const styles = StyleSheet.create({
     androidSafeArea: {
         flex: 1,
@@ -115,4 +119,3 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
     },
 });
-
