51b4fbedbcf6ad025417e4f9698e143b8da14626
Joao1721 <joaopaulolimadesouza180@gmail.com>
2024-11-05 20:47:05 -0300

    subindo

--- web/rotaCar/src/routes/HomeADM.jsx
+++ web/rotaCar/src/routes/HomeADM.jsx
@@ -1,45 +1,46 @@
 import React, { useEffect } from 'react';
 import Isotope from 'isotope-layout';
 import styleHomeAdm from './HomeADM.module.css';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const HomeADM = () => {
 
+    const navigate = useNavigate()
 
     return (
         <div className={styleHomeAdm.container}>
-            {/* Cbox 1 */}
-            <Link to="/cadastroOs">
-                <div className={styleHomeAdm.cbox}>
-                    <div className={styleHomeAdm.icon}>
-                        <i className="fa fa-code"></i>
-                    </div>
 
-                    <div className={styleHomeAdm.descr}>
-                        <div className={styleHomeAdm.fTxt}>
-                            <h3>Novo Orçamento</h3>
-                            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptates expedita corporis, culpa atque ducimus..</h4>
-                        </div>
+            <div className={styleHomeAdm.cbox} onClick={() => navigate('/cadastroOs')}>
+                <div className={styleHomeAdm.icon}>
+                    <i className="fa fa-code"></i>
+                </div>
+
+                <div className={styleHomeAdm.descr}>
+                    <div className={styleHomeAdm.fTxt}>
+                        <h3>Novo Orçamento</h3>
+                        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptates expedita corporis, culpa atque ducimus..</h4>
                     </div>
                 </div>
-            </Link>
+            </div>
 
-            {/* Cbox 2 */}
-       
-                <div className={styleHomeAdm.cbox}>
-                    <div className={styleHomeAdm.icon}>
-                        <i className="fa fa-desktop" styleHomeAdm={{ fontSize: '42px' }}></i>
-                    </div>
-                    <div className={styleHomeAdm.descr}>
-                        <div className={styleHomeAdm.fTxt}>
-                            <h3>Cadastrar um veiculo</h3>
-                            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptates expedita corporis, culpa atque ducimus..</h4>
-                        </div>
+
+            
+
+            <div className={styleHomeAdm.cbox}>
+                <div className={styleHomeAdm.icon}>
+                    <i className="fa fa-desktop" styleHomeAdm={{ fontSize: '42px' }}></i>
+                </div>
+                <div className={styleHomeAdm.descr}>
+                    <div className={styleHomeAdm.fTxt}>
+                        <h3>Cadastrar um veiculo</h3>
+                        <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptates expedita corporis, culpa atque ducimus..</h4>
                     </div>
                 </div>
-        
+            </div>
 
-            {/* Cbox 3 */}
+
+           
             <div className={styleHomeAdm.cbox}>
                 <div className={styleHomeAdm.icon}>
                     <i className="fa fa-laptop"></i>
@@ -52,7 +53,7 @@ const HomeADM = () => {
                 </div>
             </div>
 
-            {/* Cbox 4 */}
+
             <div className={styleHomeAdm.cbox}>
                 <div className={styleHomeAdm.icon}>
                     <i className="fa fa-mobile"></i>
