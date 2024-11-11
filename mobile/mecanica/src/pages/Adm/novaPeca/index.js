c341ed965b104f48cb28fd435a9fcc451a092c81
luizeduard05 <le7431474@gmail.com>
2024-11-11 10:29:38 -0300

    melhoria na estilização e adição de navegação

--- mobile/mecanica/src/pages/Adm/novaPeca/index.js
+++ mobile/mecanica/src/pages/Adm/novaPeca/index.js
@@ -7,22 +7,34 @@ import { useNavigation } from "@react-navigation/native"
 import { validaMarca, validaNome, validaValor } from "../../../utils/inputValidation"
 
 export default function NovaPeca() {
-    const navigation = useNavigation()
-    const { token } = useAuth()
+    const navigation = useNavigation();
+    const { token } = useAuth();
 
-    const [nome, setNome] = useState("")
-    const [marca, setMarca] = useState("")
+    //Campos do form
+    const [nome, setNome] = useState("");
+    const [marca, setMarca] = useState("");
     const [valor, setValor] = useState("");
 
-    const [nomeError, setNomeError] = useState(null)// Variavel responsavel por retornar o erro de validação dos inputs
-    const [marcaError, setMarcaError] = useState(null)// Variavel responsavel por retornar o erro de validação dos inputs
-    const [valorError, setValorError] = useState(null)// Variavel responsavel por retornar o erro de validação dos inputs
+    // Variaveis responsavel por retornar o erro de validação dos inputs
+    const [nomeError, setNomeError] = useState(null);
+    const [marcaError, setMarcaError] = useState(null);
+    const [valorError, setValorError] = useState(null);
 
-    const navegaVisualizaPecas = () => {
-        navigation.navigate("VisualizaPecaADM")
+    const navegaVisualizaPecas = () => { // Função para navegação de pagina
+        navigation.navigate("VisualizaPecaADM");
     }
 
-    const postPeca = async () => {
+    const navegaHome = () => {
+        navigation.navigate("Home");
+    }
+
+    const limparCamposForm = () => {
+        setNome("")
+        setMarca("")
+        setValor("")
+    }
+
+    const postPeca = async () => { // Requisição para cadastro de peça no estoque
         // Validando campos
         const nomeValidationError = validaNome(nome);
         const marcaValidationError = validaMarca(marca);
@@ -52,10 +64,8 @@ export default function NovaPeca() {
             );
             Alert.alert(`Peça ${nome} cadastrada`);
             // console.log(response.data);
-            setNome("")
-            setMarca("")
-            setValor("")
-            navegaVisualizaPecas()
+            limparCamposForm();
+            navegaVisualizaPecas();
         } catch (error) {
             console.log("Erro ao cadastrar a peça:", error);
         }
@@ -63,8 +73,8 @@ export default function NovaPeca() {
 
 
     return (
-        <LinearGradient colors={
-            ['#000000', 'rgba(0, 0, 0, 0.5)']}
+        <LinearGradient
+            colors={['#000000', 'rgba(0, 0, 0, 0.7)']}
             style={styles.androidSafeArea}
         >
             <View style={styles.container}>
@@ -74,10 +84,11 @@ export default function NovaPeca() {
                     <TextInput
                         onChangeText={(text) => {
                             setNome(text);
-                            setNomeError(null); // Limpando o erro para proxima tentativa
+                            setNomeError(null);
                         }}
                         style={[styles.inputs, nomeError ? styles.inputError : null]}
                         value={nome}
+                        placeholder="Ex: Bomba de água"
                     />
                     {nomeError && <Text style={styles.errorText}>{nomeError}</Text>}
                 </View>
@@ -87,14 +98,13 @@ export default function NovaPeca() {
                     <TextInput
                         onChangeText={(text) => {
                             setMarca(text);
-                            setMarcaError(null); // // Limpando o erro para proxima tentativa
+                            setMarcaError(null);
                         }}
                         style={[styles.inputs, marcaError ? styles.inputError : null]}
                         value={marca}
-
+                        placeholder="Ex: Indisa"
                     />
                     {marcaError && <Text style={styles.errorText}>{marcaError}</Text>}
-
                 </View>
 
                 <View style={styles.inputGroup}>
@@ -102,10 +112,12 @@ export default function NovaPeca() {
                     <TextInput
                         onChangeText={(text) => {
                             setValor(text);
-                            setValorError(null); // // Limpando o erro para proxima tentativa
+                            setValorError(null);
                         }}
                         style={[styles.inputs, valorError ? styles.inputError : null]}
                         value={valor}
+                        keyboardType="numeric"
+                        placeholder="Ex: 121.40"
                     />
                     {valorError && <Text style={styles.errorText}>{valorError}</Text>}
                 </View>
@@ -114,11 +126,10 @@ export default function NovaPeca() {
                     <Text style={styles.textBtn}>Confirmar</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity style={styles.btnCancelar}>
+                <TouchableOpacity style={styles.btnCancelar} onPress={() => { navegaHome(); limparCamposForm(); }}>
                     <Text style={styles.textBtnCancelar}>Cancelar</Text>
                 </TouchableOpacity>
             </View>
-
         </LinearGradient>
     )
 }
@@ -131,75 +142,82 @@ const styles = StyleSheet.create({
     },
     container: {
         justifyContent: "center",
-        margin: 20,
-        height: '80%',
-        backgroundColor: "#383838",
-        borderRadius: 10,
+        padding: 20,
+        marginHorizontal: 20,
+        backgroundColor: "#2d2d2d",
+        borderRadius: 12,
         alignItems: "center",
-        elevation: 7,
+        elevation: 8,
         shadowColor: '#ffffff',
         shadowOffset: {
             width: 0,
-            height: 2,
+            height: 3,
         },
         shadowOpacity: 0.8,
         shadowRadius: 6,
     },
     inputGroup: {
-        width: '90%',
+        width: '100%',
         marginBottom: 15,
     },
     label: {
-        color: "#fff",
-        fontSize: 20,
+        color: "#ffffff",
+        fontSize: 18,
+        fontWeight: '600',
         textAlign: 'left',
-        marginBottom: 5,
+        marginBottom: 8,
     },
     inputs: {
-        width: "90%",
+        width: "100%",
         height: 50,
-        backgroundColor: "#fff",
-        padding: 10,
+        backgroundColor: "#ffffff",
+        paddingHorizontal: 15,
         fontSize: 16,
-        borderRadius: 10,
-        margin: 10
+        borderRadius: 8,
+        marginTop: 5,
+        borderWidth: 1,
+        borderColor: "#cccccc",
     },
     inputError: {
         borderColor: "red",
-        borderWidth: 1,
+        borderWidth: 1.5,
     },
     errorText: {
         color: "red",
-        fontSize: 14,
+        fontSize: 13,
+        marginTop: 5,
         alignSelf: "flex-start",
-        marginLeft: 20,
     },
     btnConfirmar: {
-        width: "90%",
+        width: "100%",
         height: 50,
-        backgroundColor: "#FFF",
+        backgroundColor: "#1DB954",
         justifyContent: "center",
         alignItems: "center",
-        borderRadius: 10,
+        borderRadius: 8,
         marginTop: 20,
+        elevation: 4,
+        shadowColor: "#000",
     },
     btnCancelar: {
-        width: "90%",
+        width: "100%",
         height: 50,
-        backgroundColor: "#FF0000",
+        backgroundColor: "#d9534f",
         justifyContent: "center",
         alignItems: "center",
-        borderRadius: 10,
-        marginTop: 20,
-    },
-    textBtnCancelar: {
-        fontSize: 20,
-        color: "#fff",
-        fontWeight: "bold",
+        borderRadius: 8,
+        marginTop: 15,
+        elevation: 4,
+        shadowColor: "#000",
     },
     textBtn: {
-        fontSize: 20,
-        color: "#000",
-        fontWeight: "bold",
+        fontSize: 18,
+        color: "#ffffff",
+        fontWeight: "600",
+    },
+    textBtnCancelar: {
+        fontSize: 18,
+        color: "#ffffff",
+        fontWeight: "600",
     }
-})
+});
