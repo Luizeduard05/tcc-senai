63d0b999d69d88c615409515ae6a12779f290d39
Joao1721 <joaopaulolimadesouza180@gmail.com>
2024-11-29 21:35:35 -0300

    subindo

--- web/rotaCar/service/api.js
+++ web/rotaCar/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: "http://192.168.15.12:5000",
+    baseURL: "http://10.0.3.7:5000",
     headers: {
         'Content-Type': 'application/json',
     }
